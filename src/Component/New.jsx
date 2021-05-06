b9cafb2b251763c313ed692d88639b89c643f234
Chhavi <Chhavi.choudhary75@gmail.com>
2021-05-06 14:35:32 +0200

    create new user

--- src/Component/New.jsx
+++ src/Component/New.jsx
@@ -1,18 +1,24 @@
 import React from "react";
-
 import { useForm } from "react-hook-form";
-
+import './New.css';
 
 
 function New() {
   const { register, handleSubmit } = useForm();
-  const onSubmit = data => console.log(data);
-   
+  const onSubmit = data =>  alert(JSON.stringify(data));
+  
   return (
-    <form onSubmit={handleSubmit(onSubmit)}>
-      <input {...register("firstName")} />
-      <input type="submit" />
+    <div className="createForm">
+    <form onSubmit={handleSubmit(onSubmit)} >
+       <input type="integer" {...register("id")}placeholder= "Id" />
+      <input type="text" {...register("last_name")} placeholder= "Last Name" />
+      <input type="text" {...register("first_name")}placeholder="First Name"  />
+      <input type="text"{...register("status")} placeholder="Status" />
+      <input type="date"{...register("created_at")} placeholder="Created" />
+      <input type="date"{...register("updated_at")} placeholder="Updated" />
+      <input type="submit"/>
     </form>
+    </div>
   );
 }
 export default New;
